012e7f8e94d9ba08ee7e37c6f2b01463f649b5ef
Rafael N Siqueira <rafael.siqueira@contabilizei.com.br>
2023-11-18 16:03:46 -0300

    feat: Start project

---
+++ app/chart/page.tsx
@@ -0,0 +1,12 @@
+
+import FirstChart from "@/components/firstChart"
+
+export default function ExampleComponent() {
+  return (
+
+    <main>
+      <div id="chart"><FirstChart /></div>
+    </main>
+
+  )
+}
---
+++ utils/data.tsx
@@ -0,0 +1,17 @@
+export const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [{
+    label: '# of Votes',
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1
+  }]
+}
+
+export const options = {
+  scales: {
+    y: {
+      beginAtZero: true
+    }
+  }
+
+}
